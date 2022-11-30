2793ccde2c0fbeabe2dcd05e221e276ce1b7b979
DVMasonCooksley <mason.cooksley@data-vault.com>
2022-11-30 10:33:48 +0000

    operators_practise_1
    first half of operators, some basic understanding.

---
+++ operators_pratice.py
@@ -0,0 +1,30 @@
+x = 5
+y = 8
+
+z = x > y
+print(z)
+
+print("\n")
+
+# why the assignment will fail?
+# everything will work perfectly except for the last line (d = c + e)
+# it will not work because nothing is assigned to e
+
+# a = 1
+# b = 2
+
+# c = a + b
+# d = c + e
+
+print("\n")
+
+#n = n + 1   this will not work because n is not assigned a value
+
+#the following expression will work because n has been assigned a value
+
+n=5
+
+n = n + 1
+print(n)
+
+
