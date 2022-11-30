d990d241c7057bb954df3dc191db8e97954a5078
DVMasonCooksley <mason.cooksley@data-vault.com>
2022-11-30 15:50:40 +0000

    test on knowledge(second module)
    if you killed a alien what points you would get using if-elif-else chain.

---
+++ alien_points.py
@@ -0,0 +1,29 @@
+alien_color = ["green", "red", "yellow"]
+
+if "green" in alien_color:
+    print("5 points")
+
+#alien_color = ["green", "red", "yellow"]
+
+#if "green" in alien_color
+#    print("5 points")
+
+alien_color_1 = ["green", "red", "yellow"]
+
+if "green" in alien_color_1:
+    print("you have been awarded 5 points")
+else:
+    print("you have been awarded a total of 10 points")
+
+
+print("\n")
+
+alien_color_2 = ["red", "green", "yellow"]
+
+if "red" in alien_color_2:
+    print("you have killed a red alien you have been awarded 5 points")
+elif "yellow" in alien_color_2:
+    print("you have killed a yellow alien you have been awarded 10 points")
+else:
+    print("you have been awarded a total of 15 points")
+
